254f87dcd7117457a6b3d83d29f45b36aec4c4a9
GonzalogGauna <gonzalogaunaDev@outlook.com>
2023-07-05 16:55:00 -0300

    cambios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,17 +7,27 @@ import {
   MDBIcon,
   MDBBtn,
 } from 'mdb-react-ui-kit';
+import { Dropdown } from './Dropdown';
+import { LinksHeader } from './LinksHeader';
 
 export function Header () {
   const [showNavExternal3, setShowNavExternal3] = useState(false);
 
   return (
     <>
-      <nav class="navbar navbar-light bg-light d-none d-md-block">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">UnlAnses</a>
+      <nav className="navbar navbar-light d-none d-md-block py-4" style={{ background: "#1E90FF" }}>
+        <div className='container'>
+          <div className="container-fluid d-flex justify-content-between align-content-center">
+              <a className="navbar-brand" href="#" style={{fontWeight: 'bold', color: 'white'}}>UnlAnses</a>
+              <div className='' style={{display: 'flex', alignItems: 'center'}}>
+                {/* <div className='rounded-circle bg-black ' style={{width: '30px', height: '30px'}}>
+                </div> */}
+                <Dropdown />
+              </div>
+          </div>
         </div>
       </nav>
+      <LinksHeader />
     </>
   );
 }
--- src/components/HeaderMobile.jsx
+++ src/components/HeaderMobile.jsx
@@ -7,16 +7,17 @@ import {
   MDBIcon,
   MDBBtn,
 } from 'mdb-react-ui-kit';
+import { Dropdown } from './Dropdown';
 
 export function HeaderMobile () {
   const [showNavExternal3, setShowNavExternal3] = useState(false);
 
   return (
     <>
-      <MDBNavbar className='d-md-none' style={{ background: "#1E90FF" }}>
+      <MDBNavbar className='d-md-none py-4' style={{ background: "#1E90FF" }}>
         <MDBContainer fluid>
           <MDBNavbarToggler
-            className='ms-auto'
+            className='ms-auto m-0'
             type='button'
             data-target='#navbarToggleExternalContent'
             aria-controls='navbarToggleExternalContent'
@@ -26,19 +27,24 @@ export function HeaderMobile () {
           >
             <MDBIcon icon='bars' fas />
           </MDBNavbarToggler>
+          <div><a className="navbar-brand" style={{fontWeight: 'bold', color: 'white'}} href="#">UnlAnses</a></div>
+          <Dropdown />
         </MDBContainer>
       </MDBNavbar>
 
       <MDBCollapse show={showNavExternal3}>
         <div className='bg-light shadow-3 p-4'>
-          <MDBBtn block className='border-bottom m-0' color='link'>
-            Link 1
+          <MDBBtn block className='m-0' color='link'>
+            Educación
           </MDBBtn>
-          <MDBBtn block className='border-bottom m-0' color='link'>
-            Link 2
+          <MDBBtn block className='m-0' color='link'>
+            Trabajo
           </MDBBtn>
-          <MDBBtn block className='border-bottom m-0' color='link'>
-            Link 2
+          <MDBBtn block className='m-0' color='link'>
+            Matrimonio
+          </MDBBtn>
+          <MDBBtn block className='m-0' color='link'>
+            Carga de documentación
           </MDBBtn>
         </div>
       </MDBCollapse>
