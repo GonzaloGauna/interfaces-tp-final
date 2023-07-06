1377e3ebe9ed1eedf1682ab60caaaa4cb829d04e
GonzalogGauna <gonzalogaunaDev@outlook.com>
2023-07-06 17:01:58 -0300

    arreglitos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,10 +18,10 @@ export function Footer () {
             <p>Descarga la App</p>
             <a href="https://play.google.com/store/games?hl=es_AR&gl=US&pli=1" className="bg-black fs-6 p-3" style={{borderRadius: '50px', color: 'white'}} role="button" data-bs-toggle="button">Mi UnlAnses</a>
           </div>
-          <div><img style={{width: '100px'}} src="https://www.argentina.gob.ar/sites/default/files/argentina-presidencia.png" alt="logo argentina gobierno" /></div>
+          <div><img style={{width: '150px'}} src="https://www.argentina.gob.ar/sites/default/files/argentina-presidencia.png" alt="logo argentina gobierno" /></div>
           <div style={{margin: 'auto'}}>
 
-            <div><h3 className='p-0 m-0'>Seguinos en</h3></div>
+            <div style={{margin: 'auto'}}><h3 className='p-0 m-0'>Seguinos en</h3></div>
             <section className='p-0 f-flex' style={{justifyContent: 'center', alignContent: 'center'}}>
               <MDBBtn
                 rippleColor="dark"
