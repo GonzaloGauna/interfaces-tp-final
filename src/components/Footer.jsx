811c56b05cab123f2c7532d4bdd933609dbb7cc4
GonzalogGauna <gonzalogaunaDev@outlook.com>
2023-07-06 16:56:11 -0300

    fixxes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,7 +18,7 @@ export function Footer () {
             <p>Descarga la App</p>
             <a href="https://play.google.com/store/games?hl=es_AR&gl=US&pli=1" className="bg-black fs-6 p-3" style={{borderRadius: '50px', color: 'white'}} role="button" data-bs-toggle="button">Mi UnlAnses</a>
           </div>
-          <div><img src="..\images\Captura-removebg-preview.png" alt="logo argentina gobierno" /></div>
+          <div><img src="https://www.argentina.gob.ar/sites/default/files/argentina-presidencia.png" alt="logo argentina gobierno" /></div>
           <div style={{margin: 'auto'}}>
 
             <div><h3 className='p-0 m-0'>Seguinos en</h3></div>
